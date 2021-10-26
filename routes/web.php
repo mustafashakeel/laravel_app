c1e0fcd42a1d2938c5b1aa853d5947bb51531390
Mustafa Qureshi <mustafashakeel@gmail.com>
2021-10-26 13:36:41 -0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostsController;
 
-use App\Http\Controllers\PhotoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,41 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return '<h1> About Page </h1>';
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-// Route::get('/contact', function () {
-
-//     $respose_arr = [];
-//     $respose_arr['name'] = 'John';
-//     $respose_arr['school'] = 'Vanarts';
-//     $respose_arr['version'] = '1.2.2';
-//     return $respose_arr;
-// });
-
-// Route::get('/contact', function () {
-
-//     return view('contact');
-// });
-
-
-
-Route::get('/home', function () {
-
-    $respose_arr = [];
-    $respose_arr['name'] = 'John';
-    $respose_arr['school'] = 'Vanarts';
-    $respose_arr['version'] = '1.2.2';
-    return view('welcome', $respose_arr);
-});
-
-// Route::get('/post', [PostsController::class, 'index']);
-
-Route::resource('photos', PhotoController::class);
-
-Route::get('/post/{id}', [PostsController::class, 'index']);
-
-Route::get('/contacts/{id}', [PhotoController::class, 'contact']);
-
-Route::get('/vanarts/{id}/{student}/{city}', [PhotoController::class, 'vanarts']);
+require __DIR__.'/auth.php';
